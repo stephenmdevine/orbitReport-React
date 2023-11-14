2dd73a1b8cb864d84d7cac0229862b87d09a514b
stephenmdevine <stephenmdevine@gmail.com>
2023-11-13 18:35:55 -0600

    Through task 4 complete

---
+++ src/components/Banner.jsx
@@ -0,0 +1,10 @@
+export default function Banner () {
+    return (
+        <header>
+            Orbit Report
+            <p>
+                Click on the buttons to see the satellites in that orbit type
+            </p>
+        </header>
+    );
+}
